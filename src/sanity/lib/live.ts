f4b7fa97bc5e7f2e2564ec4b6419d2698d8031b5
Shuzaifa12 <161887664+Shuzaifa12@users.noreply.github.com>
2025-06-13 21:25:38 +0500

    Update live.ts

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
