5665ab6e3d52e81d1bdd4a77aaf4a5108fc51a47
Shuzaifa12 <161887664+Shuzaifa12@users.noreply.github.com>
2025-06-13 19:22:17 +0500

    Update page.tsx

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,35 +1,30 @@
+import React from 'react'
+import { client } from '@/sanity/lib/client'
+import Image from 'next/image'
+import { urlFor } from '@/sanity/lib/image'
 
-import { client } from "@/sanity/lib/client";
-import React from 'react';
-import Image from "next/image";
-import { urlFor } from "@/sanity/lib/image";
-
-async function Skills () {
+export default async function Skills () {
 
   const query = `*[_type == "Skills"]`;
-  const skills = await client.fetch(query, {}, { cache : 'no-cache' };
+  const skills = await client.fetch(query, {}, { cache: 'no-store' });
 
   return (
-    <>
-      <div className="my-8 px-4 sm:px-10 py-4 flex flex-wrap justify-center sm:justify-evenly md:justify-start gap-4">
-        {skills.map((s: any, index: number) => (
-          <div
-          key={index}
-          className="border-2 border-[#FA6E00] px-4 py-3 w-full sm:w-[45%] md:w-[30%] lg:w-[18%] flex flex-col items-center rounded-md "
-        >
-          <Image
-            src={urlFor(s.image).url()}
-            alt={s.name || 'Skill Image'}
-            width={60}
-            height={60}
-            className="my-2 p-1 rounded-md bg-slate-100"
-          />
-          <p className="font-bold text-lg sm:text-xl text-center break-words">{s.name}</p>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="my-8 px-4 sm:px-10 py-4 flex flex-wrap justify-center sm:justify-evenly md:justify-start gap-4">
+         {skills.map((s: any, index: number) => (
+           <div
+           key={index}
+           className="border-2 border-[#FA6E00] px-4 py-3 w-full sm:w-[45%] md:w-[30%] lg:w-[18%] flex flex-col items-center rounded-md "
+         >
+           <Image
+             src={urlFor(s.image).url()}
+             alt={s.name || 'Skill Image'}
+             width={60}
+             height={60}
+             className="my-2 p-1 rounded-md bg-slate-100"
+           />
+           <p className="font-bold text-lg sm:text-xl text-center break-words">{s.name}</p>
+           </div>
+         ))}
+     </div>
   )
-
-  export default Skills;
 }
